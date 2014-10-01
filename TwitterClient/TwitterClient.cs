ce435c71b7dbec916b5bd5b1f9ed7830c8d89ef0
nekko1119 <taku50501119@gmail.com>
2014-10-02 00:51:26 +0900

    Twitterクライアントのプロジェクトを作成

---
+++ TwitterClient/TwitterClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Twitter
+{
+    public class TwitterClient
+    {
+		private HttpClient client;
+
+		public TwitterClient()
+		{
+			client = new HttpClient();
+			client.BaseAddress = new Uri("http://api.twitter.com/1.1");
+			var query = HttpUtility.ParseQueryString(string.Empty);
+			query["hoge"] = "あ";
+			query["foo"] = "google";
+			System.Console.WriteLine(query.ToString());
+			var builder = new UriBuilder(client.BaseAddress);
+			builder.Query = query.ToString();
+			System.Console.WriteLine(builder.ToString());
+			client.GetAsync(builder.ToString());
+		}
+    }
+}
---
+++ TwitterClientTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TwitterClientTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Twitter.TwitterClient client = new Twitter.TwitterClient();
+		}
+	}
+}
