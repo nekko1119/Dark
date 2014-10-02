9735bd27e3969939d0f3d038278882c856834e56
nekko1119 <taku50501119@gmail.com>
2014-10-03 00:58:36 +0900

    UriEncodeを実装

--- TwitterClient/TwitterClient.cs
+++ TwitterClient/TwitterClient.cs
@@ -9,22 +9,41 @@ using System.Web;
 
 namespace Twitter
 {
+	/// <summary>
+	/// Twitter APIにアクセスするためのクライアントクラスです
+	/// </summary>
     public class TwitterClient
     {
 		private HttpClient client;
+		private readonly string baseAddress = "http://api.twitter.com/1.1";
+		// TODO app.configに書き出す
+		private readonly string consumerKey;
+		private readonly string consumerSecret;
 
 		public TwitterClient()
 		{
+			consumerKey = System.Configuration.ConfigurationManager.AppSettings["consumerKey"];
+			consumerSecret = System.Configuration.ConfigurationManager.AppSettings["consumerSecret"];
 			client = new HttpClient();
-			client.BaseAddress = new Uri("http://api.twitter.com/1.1");
-			var query = HttpUtility.ParseQueryString(string.Empty);
-			query["hoge"] = "あ";
-			query["foo"] = "google";
-			System.Console.WriteLine(query.ToString());
-			var builder = new UriBuilder(client.BaseAddress);
-			builder.Query = query.ToString();
-			System.Console.WriteLine(builder.ToString());
-			client.GetAsync(builder.ToString());
+			client.BaseAddress = new Uri(baseAddress);
+			//var query = HttpUtility.ParseQueryString(string.Empty);
+			//query["hoge"] = "あ";
+			//query["foo"] = "google";
+			//System.Console.WriteLine(query.ToString());
+			//var builder = new UriBuilder(client.BaseAddress);
+			//builder.Query = query.ToString();
+			//System.Console.WriteLine(builder.ToString());
+			//client.GetAsync(builder.ToString());
+		}
+
+		/// <summary>
+		/// 引数に与えられた文字列をUTF-8でURIエンコードします
+		/// </summary>
+		/// <param name="str">エンコードしたい文字列</param>
+		/// <returns>エンコードされた文字列</returns>
+		private string UriEncode(string str)
+		{
+			return HttpUtility.UrlEncode(str, Encoding.UTF8).ToUpper();
 		}
     }
 }
---
+++ TwitterClientTest/TwitterClientTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twitter;
+
+namespace TwitterClientTest
+{
+	[TestClass]
+	public class TwitterClientTest
+	{
+		Twitter.TwitterClient client;
+
+		[TestInitialize]
+		public void Initilaize()
+		{
+			client = new Twitter.TwitterClient();
+		}
+
+		[TestMethod]
+		public void URIエンコード()
+		{
+			Assert.AreEqual("%E3%81%82", client.AsDynamic().UriEncode("あ"));
+		}
+	}
+}
