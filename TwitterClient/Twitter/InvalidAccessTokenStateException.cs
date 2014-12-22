17d5397a28d3d6f3603ff17ce4961e3bf551fc72
nekko1119 <taku50501119@gmail.com>
2014-12-22 15:30:14 +0900

    GetRequestTokenに例外処理を追加

---
+++ TwitterClient/Twitter/InvalidAccessTokenStateException.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter
+{
+	/// <summary>
+	/// AccessTokenが不正の状態を表す例外です
+	/// </summary>
+	public class InvalidAccessTokenStateException
+		: InvalidOperationException
+	{
+		public InvalidAccessTokenStateException()
+			: base()
+		{
+		}
+
+		public InvalidAccessTokenStateException(string accessToken, string accessTokenSecret)
+			: base()
+		{
+			AccessToken = accessToken;
+			AccessTokenSecret = accessTokenSecret;
+		}
+
+		public InvalidAccessTokenStateException(string message)
+			: base(message)
+		{
+		}
+
+		public InvalidAccessTokenStateException(string message, string accessToken, string accessTokenSecret)
+			: base(message)
+		{
+			AccessToken = accessToken;
+			AccessTokenSecret = accessTokenSecret;
+		}
+
+		public InvalidAccessTokenStateException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		public InvalidAccessTokenStateException(
+			string message, Exception innerException, string accessToken, string accessTokenSecret)
+			: base(message, innerException)
+		{
+			AccessToken = accessToken;
+			AccessTokenSecret = accessTokenSecret;
+		}
+
+		public string AccessToken
+		{
+			get;
+			private set;
+		}
+
+		public string AccessTokenSecret
+		{
+			get;
+			private set;
+		}
+
+		public override string ToString()
+		{
+			string accessToken = "AccessToken : " + AccessToken;
+			string accessTokenSecret = "AccessTokenSecret : " + AccessTokenSecret;
+			return string.Join(" ", base.ToString(), accessToken, AccessTokenSecret);
+		}
+	}
+}
--- TwitterClientTest/TwitterClientTest.cs
+++ TwitterClientTest/TwitterClientTest.cs
@@ -1,18 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Net;
 using System.Xml.Linq;
+using Twitter;
 
 namespace TwitterTest
 {
 	[TestClass]
 	public class TwitterClientTest
 	{
-		private static Twitter.TwitterClient client;
+		private static TwitterClient client;
 
 		[ClassInitialize]
 		public static void InitilaizeClass(TestContext testContext)
 		{
-			client = new Twitter.TwitterClient
+			client = new TwitterClient
 				(
 				"r1xe4MXNfCg4aJRvmaNig5IOw",
 				"DaczKjZivPFHOZ1DnwMPCD05EomwGLMMtxqs4McU8Sm7CtPb2b"
@@ -47,11 +49,45 @@ namespace TwitterTest
 			[TestMethod]
 			public void 正常系()
 			{
-				client.AccessToken = "";
-				client.AccessTokenSecret = "";
-				var response = client.GetRequestToken().Result;
-				System.Console.WriteLine("oauth_token: {0}, oauth_token_secret: {1}, oauth_callback_confirmed: {2}", response.OAuthToken, response.OAuthTokenSecret, response.OAuthCallbackConfirmed);
-				Assert.AreEqual(HttpStatusCode.OK, response.StatusCode.Code);
+				string accessToken = client.AccessToken;
+				string accessTokenSecret = client.AccessTokenSecret;
+				try
+				{
+					client.AccessToken = "";
+					client.AccessTokenSecret = "";
+					var response = client.GetRequestToken().Result;
+					System.Console.WriteLine(
+						"oauth_token: {0}, oauth_token_secret: {1}, oauth_callback_confirmed: {2}",
+						response.OAuthToken, response.OAuthTokenSecret, response.OAuthCallbackConfirmed);
+					Assert.AreEqual(HttpStatusCode.OK, response.StatusCode.Code);
+				}
+				finally
+				{
+					client.AccessToken = accessToken;
+					client.AccessTokenSecret = accessTokenSecret;
+				}
+			}
+
+			[TestMethod]
+			public void アクセストークン設定済み()
+			{
+				try
+				{
+					var response = client.GetRequestToken().Result;
+					Assert.Fail("例外が発生しませんでした");
+				}
+				catch (AggregateException aggregateException)
+				{
+					InvalidAccessTokenStateException e =
+						aggregateException.InnerException as InvalidAccessTokenStateException;
+					Assert.IsNotNull(e);
+					System.Console.WriteLine(e);
+					Assert.IsTrue
+					(
+						!string.IsNullOrEmpty(e.AccessToken) ||
+						!string.IsNullOrEmpty(e.AccessTokenSecret)
+					);
+				}
 			}
 		}
 	}
